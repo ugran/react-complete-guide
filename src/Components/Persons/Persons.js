a42cd0daf5d75275d6030ef975f5ea35d60ae518
ugran <iraklikori@gmail.com>
2018-10-22 16:20:01 +0400

    before pull

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -15,6 +15,10 @@ class Persons extends Component {
         console.log('[Persons.js] Inside componentDidMount()');
     }
 
+    componentWillReceiveProps(nextProps) {
+        console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
+    }
+
     render () {
         console.log('[Persons.js] Inside render()');
         return this.props.persons.map( (person,index) => {
